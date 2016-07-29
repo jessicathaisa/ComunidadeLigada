2e10512b1254c8da0103da79f38bc131e2ad2370
Jessica Almeida <jessicatsalmeida@gmail.com>
2016-07-29 06:16:16 -0300

    Adicinei demais campos :)

--- comunidade-ligada/src/main/java/io/jessicathaisa/comunidadeligada/servico/Servico.java
+++ comunidade-ligada/src/main/java/io/jessicathaisa/comunidadeligada/servico/Servico.java
@@ -1,5 +1,6 @@
 package io.jessicathaisa.comunidadeligada.servico;
 
+import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,9 +11,18 @@ public class Servico extends Framework.Modelo.Servico {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Override
 	public Long getIdentificador() {
-		// TODO Auto-generated method stub
 		return super.getIdentificador();
 	}
+	
+	@Basic
+	public String getNome() {
+		return super.getNome();
+	}
+	
+	@Basic
+	public String getDescricao() {
+		return super.getDescricao();
+	}
+	
 }
